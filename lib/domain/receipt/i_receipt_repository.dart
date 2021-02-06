8ef78e7b5aa370ad3b11cc315d55ec1b58be4f42
patrickchow0803 <patrickchow0803@gmail.com>
2021-02-06 18:08:45 -0500

    Added a receipt repository interface

---
+++ lib/domain/receipt/i_receipt_repository.dart
@@ -0,0 +1,23 @@
+// I in the class name stands for Interface
+// Treat this abstract class as an Interface.
+
+// Abstract classes can have constants, members, method stubs (methods without a body)
+// whereas interfaces can only have constants and methods stubs.
+import 'package:dartz/dartz.dart';
+import 'package:finance/domain/receipt/receipt.dart';
+import 'package:finance/domain/receipt/receipt_failure.dart';
+
+abstract class IReceiptRepository {
+  // watches all of the receipts
+  // receipts come in as a stream and can either return a ReceiptFailure or a list of receipts
+  Stream<Either<ReceiptFailure, List<Receipt>>> watchAll();
+
+  // watches only favorite receipts
+  Stream<Either<ReceiptFailure, List<Receipt>>> watchFavorite();
+
+  // When creating a receipts, you can either fail when creating it
+  // or you can successfully create the receipts but nothing needs to be returned
+  Future<Either<ReceiptFailure, Unit>> create(Receipt note);
+  Future<Either<ReceiptFailure, Unit>> update(Receipt note);
+  Future<Either<ReceiptFailure, Unit>> delete(Receipt note);
+}
