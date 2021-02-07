0b4cd504c9922a589e5a6742471fefe1259e1d10
patrickchow0803 <patrickchow0803@gmail.com>
2021-02-07 13:13:19 -0500

    Added firebase_helpers to make using firestore easier and an errors file

---
+++ lib/infrastructure/core/firebase_helpers.dart
@@ -0,0 +1,22 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:finance/domain/auth/i_auth_facade.dart';
+import 'package:finance/domain/core/errors.dart';
+import 'package:finance/injection.dart';
+
+// File contains extension methods for Firestore related code
+
+// true path to get receipts is: users/{userId}/receipts/{receiptsId}
+
+// userDocument gives users/{userId}/
+extension FirestoreX on FirebaseFirestore {
+  Future<DocumentReference> userDocument() async {
+    final userOption = await getIt<IAuthFacade>().getSignedInUser();
+    final user = userOption.getOrElse(() => throw NotAuthenticatedError());
+    return FirebaseFirestore.instance.collection('users').doc(user.id);
+  }
+}
+
+extension DocumentReferenceX on DocumentReference {
+  // this is the same as just doing users('usersId').collection('receipts')
+  CollectionReference get noteCollection => collection('receipts');
+}
